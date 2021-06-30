6e87105bab922d758ee8a8c96f4894143ec1e743
esta-rossa <rajiabdou9@gmail.com>
2021-06-30 20:21:57 +0100

    fix: adding index equal connections size as outOfRange

--- srcs/Webserv.cpp
+++ srcs/Webserv.cpp
@@ -18,7 +18,7 @@ void		Webserv::addServer(int port)
 
 Connection &Webserv::operator[](int index)
 {
-	if (index > this->_conSize || index < 0)
+	if (index >= this->_conSize || index < 0)
 		throw std::out_of_range("Out of Webserv range");
 	std::vector<int>::iterator it = std::upper_bound(this->_indexTable.begin(), this->_indexTable.end(), index);
 	--it;
